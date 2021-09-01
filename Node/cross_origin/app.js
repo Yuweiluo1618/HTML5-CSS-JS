5d3052b1d6d78c35b7745c9ec3af48d66a74fc7b
Yuweiluo1618 <yuwei.luo@alumni.ubc.ca>
2021-09-01 02:20:21 -0700

    cross orgin finish

--- Node/cross_origin/app.js
+++ Node/cross_origin/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
+app.use(cors());
 app.get('/get_data', (req, res) => {
     let data = {
         name: "node.js",
@@ -8,6 +10,18 @@ app.get('/get_data', (req, res) => {
     }
     // res.send("callback({name: 'node.js', age: 11})");
     res.jsonp(data);
+});
+
+app.get('/get_data2', (req, res) => {
+    
+    let data = {
+        name: "node.js",
+        age: "11"
+    }
+
+    
+    // res.setHeader('Access-Control-Allow-Origin', '*');
+    res.send(data);
 });
 
 app.listen(3000, () => {
